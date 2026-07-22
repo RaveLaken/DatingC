37f7051fbe218ccfee82b496b69784ec9d20467d
RaveLaken <l_ena_ee@hotmail.com>
2026-07-22 13:36:45 +0800

    修改文件。

---
+++ src/storage/dataManager.ts
@@ -0,0 +1,33 @@
+var Bot = JSON.parse(variety.storageGet("Bot") || "{}" );
+var Testmode = JSON.parse(variety.storageGet("Testmode") || "false" );
+var Variety = JSON.parse(variety.storageGet("Variety") || "{}" );
+var Sign = JSON.parse(variety.storageGet("Sign") || "{}" );
+var Rule = JSON.parse(variety.storageGet("Rule") || "{}" );
+var Switch = JSON.parse(variety.storageGet("Switch") || "{}" );
+var SelfGroup = JSON.parse(variety.storageGet("SelfGroup") || "{}" );
+var Name = JSON.parse(variety.storageGet("Name") || "{}" );
+var Description = JSON.parse(variety.storageGet("Description") || "{}" );
+var Data = JSON.parse(variety.storageGet("Data") || "{}" );
+var Item = JSON.parse( variety.storageGet("Item") || "{}" );
+var Equip = JSON.parse( variety.storageGet("Equip") || "{}" );
+var Shop = JSON.parse(variety.storageGet("Shop") || "{}" );
+var Lottery = JSON.parse(variety.storageGet("Lottery") || "{}" );
+var Survey = JSON.parse(variety.storageGet("Survey") || "{}" );
+
+var Kinship = JSON.parse(variety.storageGet("Kinship") || "{}" );
+var SMS = JSON.parse(variety.storageGet("SMS") || "{}" );
+var Gift = JSON.parse(variety.storageGet("Gift") || "{}" );
+var Letter = JSON.parse(variety.storageGet("Letter") || "{}" );
+var Board = JSON.parse(variety.storageGet("Board") || "{}" );
+var Song = JSON.parse(variety.storageGet("Song") || "{}" );
+
+var GroupStat = JSON.parse(variety.storageGet("GroupStat") || "{}" );
+var Schedule = JSON.parse(variety.storageGet("Schedule") || "{}" );
+var Info = JSON.parse(variety.storageGet("Info") || "{}" );
+var Hope = JSON.parse(variety.storageGet("Hope") || "{}" );
+var Backup = JSON.parse(variety.storageGet("Backup") || "{}" );
+var Replay = JSON.parse(variety.storageGet("Replay") || "{}" );
+var Log = JSON.parse(variety.storageGet("Log") || "{}" );
+
+var List = JSON.parse(variety.storageGet("List") || "{}" );
+var Auction = JSON.parse(variety.storageGet("Auction") || "{}" );
